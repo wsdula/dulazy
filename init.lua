0c7aabf4d2b4bb2becb0500f5d2a1313d0a65174
wsdula <wsdula@gmail.com>
2024-01-08 01:59:27 -0500

    added copilot back in and linked with nvim cmp

--- init.lua
+++ init.lua
@@ -1,7 +1,1 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set relativenumber")
-
-require("config.lazy")
+require("dulazy.config.lazy")
--- plugins/completions.lua
+++ plugins/completions.lua
@@ -11,6 +11,7 @@ return {
   },
   {
     "hrsh7th/nvim-cmp",
+
     config = function()
       -- Set up nvim-cmp.
       local cmp = require("cmp")
@@ -35,6 +36,9 @@ return {
           ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         }),
         sources = cmp.config.sources({
+          -- Copilot Source
+          { name = "copilot", group_index = 2 },
+          -- Other Sources
           { name = "nvim_lsp" },
           { name = "luasnip" }, -- For luasnip users.
         }, {
