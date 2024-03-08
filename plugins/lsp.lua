76b39cb807b03e6db138a7d5e1c5245d38690655
wsdula <wsdula@gmail.com>
2024-03-07 19:50:31 -0500

    multiple updates, unsure what it all encompasses

--- plugins/lsp.lua
+++ plugins/lsp.lua
@@ -63,7 +63,7 @@ return {
       require("mason-lspconfig").setup()
 
       -- Enable the following language servers
-      --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
+     --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
       --
       --  Add any additional override configuration in the following tables. They will be passed to
       --  the `settings` field of the server config. You must look up that documentation yourself.
