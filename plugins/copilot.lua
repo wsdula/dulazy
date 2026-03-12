449d0487980933f391c5a3a30b311eae8adddefb
wsdula <wsdula@gmail.com>
2026-03-12 00:25:34 -0400

    fix: disabled copilot properly

--- plugins/copilot.lua
+++ plugins/copilot.lua
@@ -1,16 +1,18 @@
 return {
   {
-    -- "zbirenbaum/copilot.lua",
-    -- cmd = "Copilot",
-    -- event = "InsertEnter",
-    -- config = function()
-    --   require("copilot").setup({})
-    -- end,
+    "zbirenbaum/copilot.lua",
+    enabled = false,
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({})
+    end,
   },
   {
-    -- "zbirenbaum/copilot-cmp",
-    -- config = function()
-    --   require("copilot_cmp").setup()
-    -- end,
+    "zbirenbaum/copilot-cmp",
+    enabled = false,
+    config = function()
+      require("copilot_cmp").setup()
+    end,
   },
 }
